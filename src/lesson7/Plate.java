5bee234d125dc3fea9ffe0f180db8beaa751c226
Denis_Fomenkov <tgimrv@ya.ru>
2020-10-10 00:51:40 +0300

    HW for lesson 7: cats and plate with food

---
+++ src/lesson7/Plate.java
@@ -0,0 +1,29 @@
+package lesson7;
+
+public class Plate {
+
+    private int food;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public void info() {
+        System.out.println("In the plate: " + food + " units food.");
+    }
+
+    public boolean reduceAmountFood (int appetite) {
+        if (appetite <= this.food){
+            this.food -= appetite;
+            return true;
+        }
+        return false;
+    }
+
+    public void addFoodIntoPlate (int food) {
+        this.food += food;
+        System.out.println("We added " + food + " units food.");
+        info();
+    }
+
+}
